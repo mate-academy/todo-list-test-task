389746dec79a96ab8ca07ee16901dd8186bcce08
mregorlite <egor.infinity@gmail.com>
2024-11-27 11:59:56 +0200

    add pages tag: create, update, delete

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from todo_list.views import (
     HomePageView,
     TagListView,
+    tag_toggle,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView,
 )
 
 
@@ -10,5 +14,10 @@ app_name="todo-list"
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("tags", TagListView.as_view(), name="tags"),
+    path("tags/<int:pk>/toggle/", tag_toggle, name="tag-toggle"),
+    path("tags/create/", TagCreateView.as_view(), name="tag-create"),
+    path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
+    path("tags/<int:pk>/delete/", TagDeleteView.as_view(), name="tag-delete"),
+
 ]
 
--- todo_list/views.py
+++ todo_list/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from django.urls import reverse, reverse_lazy
 from django.views import generic
 
 from todo_list.models import ToDo, Tag
@@ -12,3 +14,27 @@ class HomePageView(generic.ListView):
 class TagListView(generic.ListView):
     model = Tag
     template_name = "todo_list/tags.html"
+
+
+def tag_toggle(request: HttpRequest, pk: int) -> HttpResponse:
+    todo = ToDo.objects.get(pk=pk)
+    todo.is_done = not todo.is_done
+    todo.save()
+    return redirect(reverse("todo-list:home"))
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = ["title"]
+    success_url = reverse_lazy("todo-list:tags")
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = ["title"]
+    success_url = reverse_lazy("todo-list:tags")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    fields = ["title"]
+    success_url = reverse_lazy("todo-list:tags")
